ffd8b47215b49b3e395844b427285adca043a157
captainkyle89 <45925047+captainkyle89@users.noreply.github.com>
2026-08-21 22:24:54 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,7 +1,7 @@
 const GALLERY_IMAGES = [
     "Liz-Picture.jpg",
     "Dennis-Picture.jpg",
-    "NYSUBWA-Logo.png",
+    "NYSUBWA-logo.png",
     "Olympic-Logo.jpg",
     "Team-Photo.jpg",
     "Championship-Trophy.jpg",
